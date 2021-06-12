e72dfdc7300471bef9867eb6ec23d6fdb5240f96
FredrickWampamba <fredrickwampamba1@gmail.com>
2021-06-12 20:20:11 +0300

    deleted useless files

--- add-user.php
+++ add-user.php
@@ -1,11 +1,4 @@
 <?php 
-  error_reporting(0);
-  session_start();
-
-    if (!isset($_SESSION['email'])) {
-        echo '<script type="text/javascript">window.location="index.php";</script>';
-    }
-
     if (isset($_REQUEST['add-user'])) {
       
       $password = $_REQUEST['password'];
@@ -13,12 +6,7 @@
       $username = $_REQUEST['username'];
       $userID = strtoupper(substr($username, 0,3).rand(1000,9999));
 
-      $conn = new mysqli("localhost", "root", "", "amir");
-
-      // Check connection
-      if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
+      include 'conn/conn.php';
 
       $sql = "INSERT INTO `users`(`username`, `email`, `password`, `userID`) VALUES ('$username','$email','$password','$userID')";
       if ($conn->query($sql)) {
@@ -29,42 +17,7 @@
 
     }
  ?>
-<!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <title>Add User</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-  </head>
-  <body>
-
-  <div class="container">
-    
-    <div class="col-md-12 row">
-      <h2 class="pl-10 text-center">Customer Registration System</h2>
-      <div class="col-md-3">
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link" href="dashboard.php">Dashboard</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="add-customer.php">Add Customer Info</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="view-customer.php">View Customer Information</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="users.php">Users</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="logout.php">Logout</a>
-          </li>
-        </ul>
-      </div>
+<?php include 'comp/header.php'; ?>
 
       <div class="col-md-9">
         <form method="POST">
